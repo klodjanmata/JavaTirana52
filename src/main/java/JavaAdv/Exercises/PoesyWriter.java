fac874273a82762978520c9cf101f56f6b51962a
Klodjan Mata <klodjanmata0@gmail.com>
2025-07-13 09:45:29 +0200

    Write poesy to file

---
+++ src/main/java/JavaAdv/Exercises/PoesyWriter.java
@@ -0,0 +1,31 @@
+package JavaAdv.Exercises;
+
+public class PoesyWriter {
+    private final String poesy =
+            "Në Heshtje Rritet Drita\n" +
+            "\n" +
+            "Në heshtje rritet drita e mëngjesit,\n" +
+            "si shpresë që vjen pas një nate të gjatë,\n" +
+            "çdo rreze prek me mall një shpirt të heshtur,\n" +
+            "çdo hap i ri një ëndërr sjell me vrap.\n" +
+            "\n" +
+            "Në sytë e tu ka yje që s’kanë rënë,\n" +
+            "ka ndjenja që s’kanë gjetur ende fjalë,\n" +
+            "po zemra jote di të shkruaj poezi,\n" +
+            "edhe kur bota rri në qetësi.\n" +
+            "\n" +
+            "Mos ki frikë nga nata që mbulon,\n" +
+            "as nga stuhitë që vijnë pa lajmëruar,\n" +
+            "se brenda teje frymon një diell i fshehur,\n" +
+            "që ditën tënde do dijë ta ndriçojë përsëri.";
+
+    private final String FILEPATH = "Files/Poesy.txt";
+
+    public static void main(String[] args) {
+
+    }
+
+    public void writePoesy() {
+
+    }
+}
