072c548545e541562a738266ac50d8c9acb4ca91
Klodjan Mata <klodjanmata0@gmail.com>
2025-07-19 14:24:33 +0200

    CarRentalSystem Update

--- src/main/java/CarRentalSystem/Entitiy/EngineType.java
+++ src/main/java/CarRentalSystem/Entitiy/EngineType.java
@@ -1,9 +1,17 @@
 package CarRentalSystem.Entitiy;
 
+import CarRentalSystem.Service.VehicleType;
+
+import java.util.Arrays;
+
 public enum EngineType {
     ELECTRIC,
     PETROL,
     DIESEL,
     LPG,
-    HYBRID
+    HYBRID;
+
+    public static String printAllValues() {
+        return Arrays.toString(EngineType.values());
+    }
 }
