18c6142fa30c6f6b97b2d7a922100011f3c36796
Klodjan Mata <klodjanmata0@gmail.com>
2025-09-06 12:00:01 +0200

    Hibernate exercises

---
+++ src/main/java/HibernateExercises/Util/HibernateUtil.java
@@ -0,0 +1,30 @@
+package HibernateExercises.Util;
+
+import CarRentalSystem.Entity.Client;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    public static SessionFactory buildSessionFactory() {
+        try{
+            return new Configuration()
+                    .configure("hibernateexercises.cfg.xml")
+                    .buildSessionFactory();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        sessionFactory.close();
+    }
+
+}
+
